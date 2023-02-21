cdca7cd3f6f7f1d59e6b53a6a5c48cd9f1bc73b3
SurajBhar <suraj.unisiegen@gmail.com>
2023-02-21 21:21:30 +0100

    Bash Scripts for Omni Cluster

--- slurm_scripts/run_split.sh
+++ slurm_scripts/run_split.sh
@@ -3,10 +3,9 @@
 #SBATCH -p long
 #SBATCH -o /work/ws-tmp/g051176-SB_NDF/ndf/slurm_scripts/out/%j.out
 #SBATCH -e /work/ws-tmp/g051176-SB_NDF/ndf/slurm_scripts/out/%j.err
-#SBATCH -t 5-00:00:00
-
+#SBATCH -t 10:00:00
 
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=suraj.bhardwaj@student.uni-siegen.de
 
-python /work/ws-tmp/g051176-SB_NDF/ndf/dataprocessing/preprocess.py --config /work/ws-tmp/g051176-SB_NDF/ndf/configs/shapenet_cars.txt
+python /work/ws-tmp/g051176-SB_NDF/ndf/dataprocessing/create_split.py --config /work/ws-tmp/g051176-SB_NDF/ndf/configs/shapenet_cars.txt
